0adfd4c97d969333956cbb1932287e20a5881832
João <joao.sanches7@fatec.sp.gov.br>
2023-10-09 14:44:21 -0300

    Função: Query adicionada

--- src/main/java/com/example/dslist/repositories/UserRepository.java
+++ src/main/java/com/example/dslist/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package com.example.dslist.repositories;
 
 import com.example.dslist.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    UserDetails findUserByEmail(String email);
 }
