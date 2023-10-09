f02bae09105bc3ac91a26f8c3239fd6e5b49c54e
João <joao.sanches7@fatec.sp.gov.br>
2023-10-09 14:50:41 -0300

    Refatoração: UserDetailsService renomeado

--- src/main/java/com/example/dslist/services/AuthorizationService.java
+++ src/main/java/com/example/dslist/services/AuthorizationService.java
@@ -1,23 +1,20 @@
 package com.example.dslist.services;
 
-import com.example.dslist.config.security.DsListUsersDetails;
+
 import com.example.dslist.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class DsListUserDetailsService implements UserDetailsService {
+public class AuthorizationService implements UserDetailsService {
     @Autowired
     private  UserRepository repository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return repository.findByEmail(email).map(DsListUsersDetails::new)
+        return repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
     }
 }
